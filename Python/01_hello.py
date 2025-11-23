386b50190c4a33c60131d233de5d3d45aa34bec3
Arturo1302 <235344210+Arturo1302@users.noreply.github.com>
2025-11-23 12:02:09 -0500

    repaso video python

---
+++ Python/01_hello.py
@@ -0,0 +1,9 @@
+# Hola mundo
+print ("Hola Python")
+
+
+print (type("Hola Python")) # Cadena-> str
+print (type(5)) # Entero-> int
+print (type(3.14)) # Flotante-> float
+print (type(True)) # Booleano-> bool
+print (type([1, 2, 3])) # Lista-> list
